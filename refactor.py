f6d29d7116906ceff9132a838bb3d4ecb4c0ca21
ben <ben@ascent-bcs.com>
2021-11-18 19:04:45 +0700

    Sketch out refactor commands

--- refactor.py
+++ refactor.py
@@ -3,6 +3,11 @@ import typer
 app = typer.Typer()
 
 
+@app.command()
+def unify():
+    """Unite all the nested READMEs into a single top level file"""
+
+
 @app.callback()
 def callback():
     """Transform some aspect of the current project"""
