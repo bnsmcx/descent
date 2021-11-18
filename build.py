08ecd5bd116ec2fa13e60ac166da85d01dcc83ea
ben <ben@ascent-bcs.com>
2021-11-18 17:11:31 +0700

    Sketch out build commands

--- build.py
+++ build.py
@@ -3,6 +3,21 @@ import typer
 app = typer.Typer()
 
 
+@app.command()
+def md():
+    """Output as markdown"""
+
+
+@app.command()
+def html():
+    """Output as html"""
+
+
+@app.command()
+def pdf():
+    """Output as pdf"""
+
+
 @app.callback()
 def callback():
     """Generate documentation as pdf, html, or md"""
