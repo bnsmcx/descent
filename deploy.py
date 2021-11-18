ff24ca203cc1fa7e9fb2c715044f144576882da5
ben <ben@ascent-bcs.com>
2021-11-18 18:59:11 +0700

    Sketch out deploy commands

--- deploy.py
+++ deploy.py
@@ -3,10 +3,24 @@ import typer
 app = typer.Typer()
 
 
+@app.command()
+def minimal():
+    """READMEs with headers for each file or sub-directory"""
+
+
+@app.command()
+def intelligent():
+    """READMEs that reference stuff inside files for supported languages"""
+
+
+@app.command()
+def manual():
+    """Choose exactly which sub-directories get READMEs"""
+
+
 @app.callback()
 def callback():
     """Begin your project here"""
-    pass
 
 
 if __name__ == "__main__":
